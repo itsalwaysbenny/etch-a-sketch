0772d89f91cf503fbac05ac8eceb8f6954bd68d8
Ben Taylor <taylor.ben89@gmail.com>
2022-03-30 19:10:32 +0100

    Created a function to make a grid Watched videos and a lot of Googling to help. I was making the function but not calling it so grid wasn't created

---
+++ app.js
@@ -0,0 +1,14 @@
+function makeBoard(x) {
+
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${x}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${x}, 1fr)`
+
+    for (let i = 0; i < (x * x); i++) {
+        let gridSquare = document.createElement("div");
+        board.insertAdjacentElement("beforeend", gridSquare);
+    };
+};
+
+makeBoard(200);
