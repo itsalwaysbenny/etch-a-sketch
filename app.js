c51196b72a715f84e22a57ae152e8fd7ba3f3ef4
Ben Taylor <taylor.ben89@gmail.com>
2022-04-03 19:46:00 +0100

    Trying to get gridsquares to change colour Can do on hover but struggling to get the colour to change permo Added functionality that clears the grid of all previous squares when making a new grid Also grid can only be certain size now

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 function makeBoard(x) {
-    let board = document.querySelector(".board");
-
-    board.style.gridTemplateColumns = `repeat(${x}, 1fr)`
-    board.style.gridTemplateRows = `repeat(${x}, 1fr)`
-
-    for (let i = 0; i < (x * x); i++) {
-        let gridSquare = document.createElement("div");
-        board.insertAdjacentElement("beforeend", gridSquare);
-    };
+  let board = document.querySelector(".board");
+  //deletes all squares already on the board
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${x}, 1fr)`
+  board.style.gridTemplateRows = `repeat(${x}, 1fr)`
+  for (let i = 0; i < (x * x); i++) {
+      let gridSquare = document.createElement("div");
+      gridSquare.classList.add("gridSquare");
+      board.insertAdjacentElement("beforeend", gridSquare);
+  };
 };
 
 makeBoard(16);
 
 function changeSize(value) {
-    makeBoard(value)
+  if (value < 10 || value > 100) {
+      console.log("Pick a value between 10 and 100");
+  } else {
+      makeBoard(value)
+  }
 };
