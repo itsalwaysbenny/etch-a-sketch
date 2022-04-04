615ed62489b8e28249aeb2e8ffee76f596bb3cb0
Ben Taylor <taylor.ben89@gmail.com>
2022-04-04 21:05:04 +0100

    Started to create a function to clear the board

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const gridSize = document.getElementById(["grid-size"])
+
+//I did not expect this to be the correct Id to get to change colour
+//I was expecting it to be the gridSquare id
+const mouseOver = document.getElementById("board")
+
+//reset button id
+const clearGrid = document.getElementById(["clear-grid"]);
+
+//function to create board
 function makeBoard(x) {
   let board = document.querySelector(".board");
   //deletes all squares already on the board
@@ -11,10 +21,13 @@ function makeBoard(x) {
       board.insertAdjacentElement("beforeend", gridSquare);
   };
 };
-
+//creates initial board when page starts
 makeBoard(16);
 
-function changeSize(value) {
+//allows change to number of squares in the grid
+function setGridSize(value) {
+  //clears input each time the function is run
+  gridSize.value = ""
   if (value < 10 || value > 100) {
       console.log("Pick a value between 10 and 100");
   } else {
@@ -22,12 +35,19 @@ function changeSize(value) {
   }
 };
 
-// I did not expect this to be the correct Id to get
-//I was expecting it to be the gridSquare id
-let mouseOver = document.getElementById("board")
-
+//changes colour of individual squares on mouseover
 mouseOver.addEventListener("mouseover", function(e) {
   e.target.style.backgroundColor = "blue"
 });
 
 //function to clear board
+//deletes all inner divs as in make grid function above 
+//but leaves only 1 on the board and even new make grid does not
+//seem to change the colour back
+function clearBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+}
+
+clearGrid.addEventListener("click", clearBoard);
