a8a7601070a8475dbcb029365ed1a2419a578185
Ben Taylor <taylor.ben89@gmail.com>
2022-04-06 20:36:38 +0100

    Made a function which clears the board But will keep the same grid size Will work on changing colours next

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+//sets the default grid size for starting board
+const DEAFULT_GRID_SIZE = 16;
+let currentGridSize = DEAFULT_GRID_SIZE;
+
+//setdefault for background and mouseover colours
+const DEFAULT_BACKGROUND_COLOUR = "#ffffff";
+let currentBackgroundColour = DEFAULT_BACKGROUND_COLOUR;
+const DEFAULT_MOUSEOVER_COLOUR = "#000000";
+let currentMouseoverColour = DEFAULT_MOUSEOVER_COLOUR;
+
+//button for setting the new gridsize
 const gridSize = document.getElementById(["grid-size"])
 
 //I did not expect this to be the correct Id to get to change colour
 //I was expecting it to be the gridSquare id
 const mouseOver = document.getElementById("board")
 
-//reset button id
+//reset to default button
+const resetGrid = document.getElementById(["reset-grid"]);
+
+//clear grid button
 const clearGrid = document.getElementById(["clear-grid"]);
 
 //function to create board
@@ -15,6 +29,7 @@ function makeBoard(x) {
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${x}, 1fr)`
   board.style.gridTemplateRows = `repeat(${x}, 1fr)`
+  board.style.backgroundColor = currentBackgroundColour;
   for (let i = 0; i < (x * x); i++) {
       let gridSquare = document.createElement("div");
       gridSquare.id = "gridSquare";
@@ -22,10 +37,12 @@ function makeBoard(x) {
   };
 };
 //creates initial board when page starts
-makeBoard(16);
+makeBoard(DEAFULT_GRID_SIZE)
 
 //allows change to number of squares in the grid
 function setGridSize(value) {
+  currentGridSize = value;
+  console.log(currentGridSize)
   //clears input each time the function is run
   gridSize.value = ""
   if (value < 10 || value > 100) {
@@ -37,15 +54,26 @@ function setGridSize(value) {
 
 //changes colour of individual squares on mouseover
 mouseOver.addEventListener("mouseover", function(e) {
-  e.target.style.backgroundColor = "blue"
+  e.target.style.backgroundColor = currentMouseoverColour;
 });
 
-//function to clear board
+//function to reset board
 //deletes all inner divs as in make grid function above 
 //creates a new grid with 16 same as first loading page
-function clearBoard() {
+function resetBoard() {
+  currentGridSize = DEAFULT_GRID_SIZE;
+  console.log(currentGridSize)
   board.innerHTML = "";
-  makeBoard(16)
+  makeBoard(DEAFULT_GRID_SIZE)
+};
+
+resetGrid.addEventListener("click", resetBoard);
+
+//make a function to clear grid but keep same size
+function clearBoard() {
+  console.log(currentGridSize);
+  board.innerHTML = "";
+  makeBoard(currentGridSize);
 };
 
 clearGrid.addEventListener("click", clearBoard);
